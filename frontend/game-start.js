ac2163347d141b5bb3996865e5935b988f7c3222
Jonah <donotreply@jonahkc.com>
2021-03-26 23:58:00 +0000

    why won't the buttons get css grrrrrrrr

--- frontend/game-start.js
+++ frontend/game-start.js
@@ -1,8 +1,9 @@
 var socket = io();
 
-window.onload = function() {
+//window.onload = function() {
 	socket.emit('join', sessionStorage.getItem("username"),sessionStorage.getItem("room"));
-}
+	console.log(win_button_coords);
+//}
 
 socket.on('playerData', function(isOwner) {
   console.log(isOwner);
